6b94441be3531314de6b74a10af22f7176e3ba7c
grexess <grexess@googlemail.com>
2023-05-12 16:13:22 +0200

    1 file modified, 2 added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <v-app>
-    <router-link to="/PDFPrint">PDFPrint</router-link>
-    <router-link to="/ImageWrapper">ImageWrapper</router-link>
-    <router-link to="/SimpleUpload">SimpleUpload</router-link>
-    <v-divider class="ma-8" />
-    <router-view />
+    <NavigationDrawer />
+    <ApplicationHeader />
+
+    <v-main id="mainArea">
+      <v-container fluid>
+        <router-link to="/PDFPrint">PDFPrint</router-link>
+        <router-link to="/ImageWrapper">ImageWrapper</router-link>
+        <router-link to="/SimpleUpload">SimpleUpload</router-link>
+        <v-divider class="ma-8" />
+        <router-view />
+      </v-container>
+    </v-main>
   </v-app>
 </template>
+
+<script>
+import NavigationDrawer from '@/components/framework/NavigationDrawer.vue'
+import ApplicationHeader from '@/components/framework/ApplicationHeader.vue'
+
+export default {
+  name: 'App',
+  components: { NavigationDrawer, ApplicationHeader },
+
+  data() {
+    return {}
+  },
+}
+</script>
 
 <style lang="scss">
 #app {
